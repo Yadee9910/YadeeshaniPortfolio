d8a9744a0fddc12b4a7b5ceb5f78aaf7dd563ad7
Yadeeshani Shakilanka Edirisinghe <syadeeshani@gmail.com>
2023-08-13 09:08:03 +0530

    responsiveness changed

--- script.js
+++ script.js
@@ -11,3 +11,19 @@ function toggleContent(contentId) {
     }
 }
 
+document.getElementById('menu-btn').addEventListener('click', function () {
+    var navbar = document.getElementById('navbar');
+    navbar.classList.toggle('active');
+});
+
+const menuBtn = document.getElementById('menu-btn');
+const menuIcon = document.getElementById('menu-icon');
+const navbar = document.getElementById('navbar');
+
+menuBtn.addEventListener('click', function () {
+    menuIcon.classList.toggle('fa-bars');
+    menuIcon.classList.toggle('fa-times');
+    
+});
+
+
