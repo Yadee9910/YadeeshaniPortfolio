997015eec44ebe1f9f6e7f2372660409b85c75f2
Yadeeshani Shakilanka Edirisinghe <syadeeshani@gmail.com>
2023-08-13 18:45:05 +0530

    the cleaness of the code was changed and the unecessary images were removed

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
 // JavaScript function to toggle academic and curriculum content visibility
 
-// function toggleContent(contentId) {
-    
-//     var content = document.getElementById(contentId);
-//     console.log(content)
-//     if (content.style.display === 'none') {
-//         content.style.display = 'block';
-//     } else {
-//         content.style.display = 'none';
-//     }
-// }
-
 function toggleContent(contentId) {
     
     var content = document.getElementById(contentId);
@@ -20,22 +9,6 @@ function toggleContent(contentId) {
         content.style.maxHeight = content.scrollHeight + "px";
     }
 }
-
-
-// document.getElementById('menu-btn').addEventListener('click', function () {
-//     var navbar = document.getElementById('navbar');
-//     navbar.classList.toggle('active');
-// });
-
-// const menuBtn = document.getElementById('menu-btn');
-// const menuIcon = document.getElementById('menu-icon');
-// const navbar = document.getElementById('navbar');
-
-// menuBtn.addEventListener('click', function () {
-//     menuIcon.classList.toggle('fa-bars');
-//     menuIcon.classList.toggle('fa-times');
-    
-// });
 
 document.addEventListener("DOMContentLoaded", function () {
     const menuButton = document.getElementById("menu-btn");
@@ -51,7 +24,7 @@ document.addEventListener("DOMContentLoaded", function () {
         item.addEventListener("click", function () {
             navbar.classList.remove("active"); // Close the menu
             menuIcon.classList.remove("fa-times"); // Reset the menu icon
-        git git     menuIcon.classList.add("fa-bars");
+            menuIcon.classList.add("fa-bars");
         });
     });
 });
